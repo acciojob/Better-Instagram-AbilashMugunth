87167b9d9256b69b8a07a3891abe49faf95f538e
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 11:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your code here
+let dropInfo = {
+  dragElement: null,
+  referenceElement: null,
+};
+
+let images = document.querySelectorAll(".image");
+let imagesContainer = document.querySelector("#parent");
+
+images.forEach((image) => {
+  image.addEventListener("dragstart", (e) => {
+    dropInfo.dragElement = e.target;
+    dropInfo.referenceElement = e.target.nextElementSibling;
+    console.log(dropInfo);
+  });
+
+  image.addEventListener("dragover", (e) => {
+    if (e.target == dropInfo.dragElement) {
+      return;
+    }
+
+    e.preventDefault();
+  });
+
+  image.addEventListener("drop", (e) => {
+    e.preventDefault();
+    let drop = e.target;
+
+    if (drop === dropInfo.dragElement) {
+      return;
+    }
+
+    imagesContainer.replaceChild(dropInfo.dragElement, drop);
+
+    if (drop == dropInfo.referenceElement) {
+      imagesContainer.insertBefore(drop, dropInfo.dragElement);
+    } else {
+      imagesContainer.insertBefore(drop, dropInfo.referenceElement);
+    }
+  });
+});
